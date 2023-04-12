ac0b32b545d671dc1b64309c5acca748121191f6
Binh <benjamin.vo.292003@gmail.com>
2023-04-12 17:54:12 +0700

    win lose diag

--- gameCaro/Game.cpp
+++ gameCaro/Game.cpp
@@ -499,6 +499,7 @@ bool Game::IsEndVertical() {
 bool Game::IsEndPrimeDiag() {
 	int topDiag = 0, bottomDiag = 0;
 	int XO = 0;
+	int count = 0;
 	if (!_turnX) {
 		XO = 1;
 	}
@@ -508,6 +509,8 @@ bool Game::IsEndPrimeDiag() {
 
 	for (int i = 0; i <= _pointRow; i++) {
 		if (XO == _board->CheckContain(_pointRow - i, _pointColumn - i)) {
+			winPos[count] = _board->_arrPoint[_pointColumn - i][_pointRow - i];
+			count++;
 			topDiag++;
 		}
 		else {
@@ -517,6 +520,8 @@ bool Game::IsEndPrimeDiag() {
 
 	for (int i = 1; i < _size - _pointRow; i++) {
 		if (XO == _board->CheckContain(_pointRow + i, _pointColumn + i)) {
+			winPos[count] = _board->_arrPoint[_pointColumn + i][_pointRow + i];
+			count++;
 			bottomDiag++;
 		}
 		else {
@@ -525,28 +530,6 @@ bool Game::IsEndPrimeDiag() {
 	}
 
 	if (bottomDiag + topDiag >= 5) {
-
-		int count = 0;
-		for (int i = 0; i <= _pointRow; i++) {
-			if (XO == _board->CheckContain(_pointRow - i, _pointColumn - i)) {
-				winPos[count] = _board->_arrPoint[_pointColumn - i][_pointRow - i];
-				count++;
-			}
-			else {
-				break;
-			}
-		}
-
-		for (int i = 1; i < _size - _pointRow; i++) {
-			if (XO == _board->CheckContain(_pointRow + i, _pointColumn + i)) {
-				winPos[count] = _board->_arrPoint[_pointColumn - i][_pointRow + i];
-				count++;
-			}
-			else {
-				break;
-			}
-		}
-
 		return true;
 	}
 	return false;
@@ -555,6 +538,7 @@ bool Game::IsEndPrimeDiag() {
 bool Game::IsEndSecondDiag() {
 	int topDiag = 0, bottomDiag = 0;
 	int XO = 0;
+	int count = 0;
 	if (!_turnX) {
 		XO = 1;
 	}
@@ -564,6 +548,8 @@ bool Game::IsEndSecondDiag() {
 
 	for (int i = 0; i <= _pointRow; i++) {
 		if (XO == _board->CheckContain(_pointRow - i, _pointColumn + i)) {
+			winPos[count] = _board->_arrPoint[_pointColumn + i][_pointRow - i];
+			count++;
 			topDiag++;
 		}
 		else {
@@ -573,6 +559,8 @@ bool Game::IsEndSecondDiag() {
 
 	for (int i = 1; i < _size - _pointRow; i++) {
 		if (XO == _board->CheckContain(_pointRow + i, _pointColumn - i)) {
+			winPos[count] = _board->_arrPoint[_pointColumn - i][_pointRow + i];
+			count++;
 			bottomDiag++;
 		}
 		else {
@@ -581,27 +569,6 @@ bool Game::IsEndSecondDiag() {
 	}
 
 	if (bottomDiag + topDiag >= 5) {
-		int count = 0;
-		for (int i = 0; i <= _pointRow; i++) {
-			if (XO == _board->CheckContain(_pointRow - i, _pointColumn + i)) {
-				winPos[count] = _board->_arrPoint[_pointColumn + i][_pointRow - i];
-				count++;
-			}
-			else {
-				break;
-			}
-		}
-
-		for (int i = 1; i < _size - _pointRow; i++) {
-			if (XO == _board->CheckContain(_pointColumn - i, _pointColumn - i)) {
-				winPos[count] = _board->_arrPoint[_pointColumn - i][_pointColumn - i];
-				count++;
-			}
-			else {
-				break;
-			}
-		}
-
 		return true;
 	}
 	return false;
